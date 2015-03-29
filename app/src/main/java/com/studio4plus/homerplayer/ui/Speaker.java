df4345e0a8fa410ebc2577dfbbb50c6033637c70
Marcin Simonides <marcin@studio4plus.com>
2015-03-29 13:19:38 +0200

    Read audio book titles with a softer voice.

--- app/src/main/java/com/studio4plus/homerplayer/ui/Speaker.java
+++ app/src/main/java/com/studio4plus/homerplayer/ui/Speaker.java
@@ -3,18 +3,25 @@ package com.studio4plus.homerplayer.ui;
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 class Speaker implements TextToSpeech.OnInitListener {
 
+    // TTS is usually much louder than a regular audio book recording.
+    private static final String TTS_VOLUME_ADJUSTMENT = "0.5";
+
     private final Locale locale;
     private final TextToSpeech tts;
+    private final HashMap<String, String> speechParams = new HashMap<>();
+
     private boolean ttsReady;
     private String pendingSpeech;
 
     Speaker(Context context) {
         this.locale = context.getResources().getConfiguration().locale;
         this.tts = new TextToSpeech(context, this);
+        speechParams.put(TextToSpeech.Engine.KEY_PARAM_VOLUME, TTS_VOLUME_ADJUSTMENT);
     }
 
     @Override
@@ -22,14 +29,14 @@ class Speaker implements TextToSpeech.OnInitListener {
         if (status == TextToSpeech.SUCCESS) {
             ttsReady = true;
             tts.setLanguage(locale);
-            tts.speak(pendingSpeech, TextToSpeech.QUEUE_FLUSH, null);
+            tts.speak(pendingSpeech, TextToSpeech.QUEUE_FLUSH, speechParams);
             pendingSpeech = null;
         }
     }
 
     public void speak(String text) {
         if (ttsReady) {
-            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, speechParams);
         } else {
             pendingSpeech = text;
         }
