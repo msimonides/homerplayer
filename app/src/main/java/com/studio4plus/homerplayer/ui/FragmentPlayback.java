30af5d4561950bbc71fa15b9340e5a931cea0613
Marcin Simonides <marcin@studio4plus.com>
2015-04-06 11:05:50 +0200

    Put big "Playback in progress" on the playback screen.

--- app/src/main/java/com/studio4plus/homerplayer/ui/FragmentPlayback.java
+++ app/src/main/java/com/studio4plus/homerplayer/ui/FragmentPlayback.java
@@ -6,46 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.studio4plus.homerplayer.HomerPlayerApplication;
 import com.studio4plus.homerplayer.R;
-import com.studio4plus.homerplayer.events.CurrentBookChangedEvent;
-import com.studio4plus.homerplayer.model.AudioBookManager;
-
-import de.greenrobot.event.EventBus;
 
 public class FragmentPlayback extends Fragment {
-
-    private TextView titleTextView;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater,
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_playback, container, false);
-
-        AudioBookManager audioBookManager = HomerPlayerApplication.getAudioBookManager();
-
-        titleTextView = (TextView) view.findViewById(R.id.title);
-        if (audioBookManager.getCurrentBook() != null)
-            titleTextView.setText(audioBookManager.getCurrentBook().getTitle());
-
-        EventBus.getDefault().register(this);
-
-        return view;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        EventBus.getDefault().unregister(this);
-    }
-
-    @SuppressWarnings("UnusedDeclaration")
-    public void onEvent(CurrentBookChangedEvent event) {
-        if (titleTextView != null)
-            titleTextView.setText(event.audioBook.getTitle());
+        return inflater.inflate(R.layout.fragment_playback, container, false);
     }
 }
