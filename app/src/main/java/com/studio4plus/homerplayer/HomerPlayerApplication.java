d55ad4ee756ec31bd8c0544dcc7e5becb981d7ad
Marcin Simonides <marcin@studio4plus.com>
2015-07-04 18:55:17 +0200

    Enable Crashlytics.

--- app/src/main/java/com/studio4plus/homerplayer/HomerPlayerApplication.java
+++ app/src/main/java/com/studio4plus/homerplayer/HomerPlayerApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Handler;
 import android.provider.MediaStore;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
 
 public class HomerPlayerApplication extends Application {
 
@@ -15,6 +19,9 @@ public class HomerPlayerApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build();
+        Fabric.with(this, new Crashlytics.Builder().core(core).build());
 
         component = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this))
