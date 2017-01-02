6d278c3491ace054d25061d4020124aef968aea9
Marcin Simonides <marcin@studio4plus.com>
2017-01-02 23:21:00 +0100

    Read /proc/mounts to find more removable media.
    Scans all mount points mounted in /storage/ in order to find more
    removable media than is possible with Context.getExternalFilesDirs().
    This approach is far from perfect and will fail if SD card is mounted in
    some other directory.
    
    Fixes #13.

--- app/src/main/java/com/studio4plus/homerplayer/util/FilesystemUtil.java
+++ app/src/main/java/com/studio4plus/homerplayer/util/FilesystemUtil.java
@@ -4,13 +4,67 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FilesystemUtil {
 
     public static List<File> listRootDirs(Context context) {
+        List<File> rootDirs = listStorageMounts();
+        for (File rootDir : listSemiPermanentRootDirs(context)) {
+            if (!rootDirs.contains(rootDir))
+                rootDirs.add(rootDir);
+        }
+
+        return rootDirs;
+    }
+
+    private static File getFSRootForPath(File path) {
+        while (path != null && path.isDirectory()) {
+            long fsSize = path.getTotalSpace();
+            File parent = path.getParentFile();
+            if (parent == null || parent.getTotalSpace() != fsSize)
+                return path;
+            path = parent;
+        }
+        return path;
+    }
+
+    // Returns all system mount points that start with /storage.
+    // This is likely to list attached SD cards, including those that are hidden from
+    // Context.getExternalFilesDir()..
+    // Some of the returned files may not be accessible due to permissions.
+    private static List<File> listStorageMounts() {
+        List<File> mounts = new ArrayList<>();
+        File mountsFile = new File("/proc/mounts");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(mountsFile));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(" +");
+                if (fields.length >= 2 && fields[1].startsWith("/storage")) {
+                    mounts.add(new File(fields[1]));
+                }
+            }
+        } catch (IOException e) {
+            // Ignore, just return as much as is accumulated in mounts.
+        }
+        return mounts;
+    }
+
+    // Returns a list of file system roots on all semi-permanent storage mounts.
+    // Semi-permanent storage is removable medium that is part of the device (e.g. an SD slot
+    // inside the battery compartment) and therefore unlikely to be removed often.
+    // Storage medium that is easily accessible by the user (e.g. an external SD card slot) is
+    // treated as portable storage.
+    // The Context.getExternalFilesDir() method only lists semi-permanent storage devices.
+    //
+    // See http://source.android.com/devices/storage/traditional.html#multiple-external-storage-devices
+    private static List<File> listSemiPermanentRootDirs(Context context) {
         File[] filesDirs;
         if (Build.VERSION.SDK_INT < 19)
             filesDirs = new File[]{ context.getExternalFilesDir(null) };
@@ -24,17 +78,6 @@ public class FilesystemUtil {
                 rootDirs.add(root);
         }
         return rootDirs;
-    }
-
-    private static File getFSRootForPath(File path) {
-        while (path != null && path.isDirectory()) {
-            long fsSize = path.getTotalSpace();
-            File parent = path.getParentFile();
-            if (parent == null || parent.getTotalSpace() != fsSize)
-                return path;
-            path = parent;
-        }
-        return path;
     }
 
     @TargetApi(19)
