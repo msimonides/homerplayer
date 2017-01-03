000ffcb5a31075df8333475d7b89b5ebee30acc5
Marcin Simonides <marcin@studio4plus.com>
2017-01-03 21:26:29 +0100

    Reduce the audiobooks rescan delay to 5s.

--- app/src/main/java/com/studio4plus/homerplayer/MediaStoreUpdateObserver.java
+++ app/src/main/java/com/studio4plus/homerplayer/MediaStoreUpdateObserver.java
@@ -7,9 +7,17 @@ import com.studio4plus.homerplayer.events.MediaStoreUpdateEvent;
 
 import de.greenrobot.event.EventBus;
 
+/**
+ * Observe for changes to media files and post a MediaStoreUpdateEvent to the event bus to trigger
+ * a scan for new audiobooks.
+ *
+ * The onChange method may be called a number of times as media files are being changed on the
+ * device. To avoid rescanning often, a rescan is triggered only after RESCAN_DELAY_MS milliseconds
+ * have passed since the last onChange call.
+ */
 public class MediaStoreUpdateObserver extends ContentObserver {
 
-    private static final int RESCAN_DELAY_MS = 15000;
+    private static final int RESCAN_DELAY_MS = 5000;
 
     private final Handler mainThreadHandler;
 
