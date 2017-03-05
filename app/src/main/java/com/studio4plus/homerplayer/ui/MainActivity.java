c023063be2ed31dec4b8f920dc02038fae827ab1
Marcin Simonides <marcin@studio4plus.com>
2017-03-05 09:05:18 +0100

    Enable full screen for full kiosk mode.
    Just to additionally hide the navigation bar.

--- app/src/main/java/com/studio4plus/homerplayer/ui/MainActivity.java
+++ app/src/main/java/com/studio4plus/homerplayer/ui/MainActivity.java
@@ -165,7 +165,7 @@ public class MainActivity extends BaseActivity {
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            if (globalSettings.isSimpleKioskModeEnabled() &&
+            if (globalSettings.isAnyKioskModeEnabled() &&
                     Build.VERSION.SDK_INT >= 19) {
                 visibilitySetting |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_FULLSCREEN |
