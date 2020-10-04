31b5faca48e850bbcb013a6b385ea01f2cebf049
Antoine Guillien <sladix@gmail.com>
2020-10-04 13:04:03 +0200

    Changed samples to cc ones

--- app/src/main/java/com/studio4plus/homerplayer/SamplesMap.java
+++ app/src/main/java/com/studio4plus/homerplayer/SamplesMap.java
@@ -8,7 +8,7 @@ import androidx.core.os.ConfigurationCompat;
 
 public class SamplesMap {
     private static final String EN_SAMPLES_URL = "https://homer-player.firebaseapp.com/samples.zip";
-    private static final String FR_SAMPLES_URL = "https://cdn.glitch.com/5b1a0f47-cb72-4db8-8ea3-9aa2d6fbc0c5%2Fsamples-fr.zip?v=1601768859924";
+    private static final String FR_SAMPLES_URL = "https://cdn.glitch.com/5b1a0f47-cb72-4db8-8ea3-9aa2d6fbc0c5%2Fsamples-fr.zip?v=1601809045024";
 
     private Map<String, String> map;
 
