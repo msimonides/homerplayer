da778c67cc22c3d8e09daf3a619ab1bb64a3c0af
Marcin Simonides <marcin@studio4plus.com>
2022-03-12 15:36:44 +0100

    Improve scanning speed.

--- app/src/main/java/com/studio4plus/homerplayer/filescanner/ScanDocumentTreeTask.java
+++ app/src/main/java/com/studio4plus/homerplayer/filescanner/ScanDocumentTreeTask.java
@@ -1,22 +1,55 @@
 package com.studio4plus.homerplayer.filescanner;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.SystemClock;
+import android.provider.DocumentsContract;
 import android.util.Base64;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.documentfile.provider.DocumentFile;
+import androidx.annotation.Nullable;
+
+import com.studio4plus.homerplayer.util.CollectionUtils;
 
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.Callable;
 
 public class ScanDocumentTreeTask implements Callable<List<FileSet>> {
+
+    private static final String[] DOCUMENTS_PROJECTION = new String[]{
+            DocumentsContract.Document.COLUMN_DOCUMENT_ID,
+            DocumentsContract.Document.COLUMN_DISPLAY_NAME,
+            DocumentsContract.Document.COLUMN_MIME_TYPE,
+            DocumentsContract.Document.COLUMN_SIZE
+    };
+    private static final String SORT_BY_NAME =
+             DocumentsContract.Document.COLUMN_DISPLAY_NAME + " ASC";
+
+    private static class FileItem {
+        public final String path;
+        public final Uri uri;
+        public final long size;
+
+        private FileItem(@NonNull String path, @NonNull Uri uri, long size) {
+            this.path = path;
+            this.uri = uri;
+            this.size = size;
+        }
+
+        @NonNull
+        public String getName() {
+            int index = path.lastIndexOf("/");
+            return index > -1 ? path.substring(index + 1) : path;
+        }
+    }
 
     @NonNull
     private final Context context;
@@ -33,58 +66,80 @@ public class ScanDocumentTreeTask implements Callable<List<FileSet>> {
     @Override
     public List<FileSet> call() throws Exception {
         List<FileSet> filesets = new ArrayList<>();
+        long start = SystemClock.elapsedRealtime();
         for (Uri uri : audiobooksFolderUris) {
-            DocumentFile folder = DocumentFile.fromTreeUri(context, uri);
-            filesets.addAll(scanAudiobooks(folder.listFiles()));
+            filesets.addAll(scanAudiobooks(uri));
         }
+        Collections.sort(filesets, (a, b) -> a.name.compareTo(b.name));
+        long end = SystemClock.elapsedRealtime();
+        Log.d("ListTest", "time: " + (end - start) + "ms");
         return filesets;
     }
 
-    private List<FileSet> scanAudiobooks(@NonNull DocumentFile[] audiobookFolders) {
-        List<FileSet> fileSets = new ArrayList<>(audiobookFolders.length);
-        for (DocumentFile audiobookFolder : audiobookFolders) {
-            List<DocumentFile> audioFiles = new ArrayList<>();
-            addFilesRecursive(audioFiles, audiobookFolder); // TODO: test DocumentContract.buildChildDocumentsUriUsingTree
+    private List<FileSet> scanAudiobooks(@NonNull Uri treeUri) {
+        Uri childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(
+                treeUri, DocumentsContract.getTreeDocumentId(treeUri));
 
-            if (audioFiles.size() > 0) {
-                ByteBuffer bufferLong = ByteBuffer.allocate(Long.SIZE);
-                try {
-                    List<Uri> uris = new ArrayList<>(audioFiles.size());
-                    MessageDigest digest = MessageDigest.getInstance("MD5");
-                    for (DocumentFile file : audioFiles) {
-                        uris.add(file.getUri());
+        List<FileSet> fileSets = new ArrayList<>();
+        try(Cursor cursor =
+                    context.getContentResolver().query(childrenUri, DOCUMENTS_PROJECTION, null, null, null)) {
+            while(cursor.moveToNext()) {
+                if (isFolder(cursor.getString(2))) {
+                    String name = cursor.getString(1);
+                    List<FileItem> files = new ArrayList<>();
+                    scanAudiobook(treeUri, cursor.getString(0), files, name);
 
-                        String parentName = file.getParentFile() != null ? file.getParentFile().getName() : null;
-                        parentName = parentName != null ? parentName : "";
-                        String name = file.getName() != null ? file.getName() : "";
-
-                        // TODO: what if the same book is in two directories?
-                        bufferLong.putLong(0, file.length());
-                        digest.update(parentName.getBytes());
-                        digest.update(name.getBytes());
-                        digest.update(bufferLong);
+                    if (!files.isEmpty()) {
+                        Collections.sort(files, (a, b) -> a.path.compareTo(b.path));
+                        Uri[] uris = CollectionUtils.map(files, f -> f.uri).toArray(new Uri[]{});
+                        fileSets.add(new FileSet(generateId(files), name, uris, false));
                     }
-                    String id = Base64.encodeToString(digest.digest(), Base64.NO_PADDING | Base64.NO_WRAP);
-                    String name = Objects.requireNonNull(audiobookFolder.getName());
-                    fileSets.add(new FileSet(id, name, uris.toArray(new Uri[]{}), false));
-                } catch (NoSuchAlgorithmException e) {
-                    // Never happens.
-                    e.printStackTrace();
-                    throw new RuntimeException("MD5 not available");
                 }
             }
         }
         return fileSets;
     }
 
-    private void addFilesRecursive(@NonNull List<DocumentFile> audioFiles, @NonNull DocumentFile folder) {
-        DocumentFile[] files = folder.listFiles();
-        for (DocumentFile file : files) {
-            if (file.isDirectory()) {
-                addFilesRecursive(audioFiles, file);
-            } else if (file.getType() != null && file.getType().startsWith("audio/")) {
-                audioFiles.add(file);
+    private void scanAudiobook(
+            @NonNull Uri rootUri,
+            @NonNull String folderDocId,
+            @NonNull List<FileItem> files,
+            @NonNull String path) {
+        Uri childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(rootUri, folderDocId);
+
+        try(Cursor cursor =
+                    context.getContentResolver().query(childrenUri, DOCUMENTS_PROJECTION, null, null, SORT_BY_NAME)) {
+            while(cursor.moveToNext()) {
+                String filePath = path + "/" + cursor.getString(1);
+                String documentId = cursor.getString(0);
+                String mimeType = cursor.getString(2);
+                if (isFolder(mimeType)) {
+                    scanAudiobook(rootUri, documentId, files, filePath);
+                } else if (mimeType.startsWith("audio/")) {
+                    long size = cursor.getLong(3);
+                    Uri uri = DocumentsContract.buildDocumentUriUsingTree(rootUri, documentId);
+                    files.add(new FileItem(filePath, uri, size));
+                }
             }
         }
+    }
+
+    private String generateId(@NonNull List<FileItem> files) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            ByteBuffer bufferLong = ByteBuffer.allocate(Long.SIZE);
+            for (FileItem file : files) {
+                bufferLong.putLong(0, file.size);
+                digest.update(file.path.getBytes());
+                digest.update(bufferLong);
+            }
+            return Base64.encodeToString(digest.digest(), Base64.NO_PADDING | Base64.NO_WRAP);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e); // Should never happen.
+        }
+    }
+
+    private static boolean isFolder(@Nullable String mimeType) {
+        return DocumentsContract.Document.MIME_TYPE_DIR.equals(mimeType);
     }
 }
