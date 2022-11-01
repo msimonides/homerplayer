e8d6dece8eb8b6d3cc4c1cccdbfee6c962df97a5
Marcin Simonides <marcin@studio4plus.com>
2022-11-01 16:11:46 +0100

    Update targetSdkVersion to 31.

--- app/src/main/java/com/studio4plus/homerplayer/service/NotificationUtil.java
+++ app/src/main/java/com/studio4plus/homerplayer/service/NotificationUtil.java
@@ -7,6 +7,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
 import androidx.core.app.NotificationCompat;
 
 import com.google.common.base.Preconditions;
@@ -19,13 +21,15 @@ public class NotificationUtil {
     // it's better to reuse the channel instead of providing another one just for
     // one-time action.
     private static final String PLAYBACK_SERVICE_CHANNEL_ID = "playback";
+    private static final int FLAG_IMMUTABLE =
+            Build.VERSION.SDK_INT < 23 ? 0 : PendingIntent.FLAG_IMMUTABLE;
 
     static Notification createForegroundServiceNotification(
             Context context, int stringId, int drawableId) {
         Intent activityIntent = new Intent(context, MainActivity.class);
         activityIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         PendingIntent intent = PendingIntent.getActivity(
-                context, 0, activityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                context, 0, activityIntent, PendingIntent.FLAG_UPDATE_CURRENT | FLAG_IMMUTABLE);
 
         return new NotificationCompat.Builder(context, PLAYBACK_SERVICE_CHANNEL_ID)
                 .setContentTitle(context.getResources().getString(stringId))
